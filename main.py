ecf66be946b7e8ca2657c66873e1d7d4c8b52521
perttuvep <perttuvep@proton.me>
2024-12-26 16:04:33 +0200

    char count+restructuring

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
+def wc(str):
+    count = 0
+    for word in str:
+        count += 1
+    return count
+
+def charcounter(str:str):
+    chardict = {}
+    for char in str:
+        if char.isalpha():
+            chardict[char.lower()] = chardict.get(char.lower(),0) + 1
+    return chardict
+
 def main():
     with open("./books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
-
-        wordcount = 0
-        words = file_contents.split()
-
-        character_count = {}
-
-        for word in words:
-            wordcount += 1
-            for char in word:
-                character_count[char.lower()] += 1
-
-        print(wordcount)
-        print(character_count)
-
+    
+        word_count = wc(file_contents)
+        char_count = charcounter(file_contents)
+        print(word_count,char_count)
+            
 
         
 main()
